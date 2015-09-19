5db69a7412770c25c686bcf3be8b32cabd84d4a7
Andi Palo <sanandrea8080@gmail.com>
2015-09-19 10:04:09 +0200

    initial commit

---
+++ CrossTranslator/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  CrossTranslator
+//
+//  Created by Andi Palo on 19/09/15.
+//  Copyright © 2015 Andi Palo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
+
---
+++ CrossTranslator/MasterViewController.h
@@ -0,0 +1,23 @@
+//
+//  MasterViewController.h
+//  CrossTranslator
+//
+//  Created by Andi Palo on 19/09/15.
+//  Copyright © 2015 Andi Palo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+
+@end
+
